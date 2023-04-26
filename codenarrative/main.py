bae2361fb2bdd557c57f16313f05d958a4b79b67
Nikolay Antipov <nick.antipov@gmail.com>
2023-04-26 14:44:07 +0000

    Fix spelling

--- codenarrative/main.py
+++ codenarrative/main.py
@@ -1,6 +1,6 @@
 import sys
 import service.scene
-import service.redering
+import service.rendering
 
 
 def main():
@@ -12,7 +12,7 @@ def main():
     if len(sys.argv) >= 3:
         profile_name = sys.argv[2]
 
-    service.redering.render(service.scene.read_scene_file(sys.argv[1]), profile_name)
+    service.rendering.render(service.scene.read_scene_file(sys.argv[1]), profile_name)
 
 
 if __name__ == "__main__":
