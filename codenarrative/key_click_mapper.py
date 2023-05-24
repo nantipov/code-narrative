3c6b5d173c039b845160125e233638b3cb6a1afc
Nikolay Antipov <nick.antipov@gmail.com>
2023-05-24 22:02:23 +0200

    Keyboard typing audio file

---
+++ codenarrative/key_click_mapper.py
@@ -0,0 +1,24 @@
+INPUT_WAV_FILENAME = "sounds/typing_audio.wav"
+OUTPUT_MAPPING_FILENAME = "sounds/typing_audio.csv"
+
+
+def main():
+    print()
+
+
+def check_format():
+    # todo
+    # check riff
+    # check wave
+    # check fmt
+    # type pcm (no compression)
+    # write format data into csv file, so later build audio file based on this
+    print()
+
+
+if __name__ == "__main__":
+    main()
+
+# https://en.wikipedia.org/wiki/WAV
+# https://de.wikipedia.org/wiki/RIFF_WAVE
+# https://docs.fileformat.com/audio/wav/
